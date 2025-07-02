4c4567cbf2b82e67e87a162023431928a79ffcec
arindam roy <arindamroy@arindams-Mac-mini.local>
2025-07-03 01:58:30 +0530

    add rainbowkitprovider

--- components/SwapProvider.tsx
+++ components/SwapProvider.tsx
@@ -2,7 +2,9 @@
 
 import { WagmiProvider } from "wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { config } from "@/lib/wagmi";
+import "@rainbow-me/rainbowkit/styles.css";
 
 const queryClient = new QueryClient();
 
@@ -11,7 +13,9 @@ const SwapProvider: React.FC<{
 }> = ({ children }) => {
   return (
     <QueryClientProvider client={queryClient}>
-      <WagmiProvider config={config}>{children}</WagmiProvider>
+      <WagmiProvider config={config}>
+        <RainbowKitProvider>{children}</RainbowKitProvider>
+      </WagmiProvider>
     </QueryClientProvider>
   );
 };
