bf6ccd7b89ae95abbb9afe5acdd34ad2b6dc802d
arindam roy <arindamroy@arindams-Mac-mini.local>
2025-07-02 15:39:12 +0530

    create the home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/Footer";
+import { Toaster } from "@/components/Toaster";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="mx-auto min-h-screen bg-[#FFC882] relative overflow-hidden">
+          {children}
+          <Footer />
+          <Toaster />
+        </div>
       </body>
     </html>
   );
