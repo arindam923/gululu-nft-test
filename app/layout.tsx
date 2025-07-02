d7142391e4264fb105db023ff5616af1c1c95e11
arindam roy <arindamroy@arindams-Mac-mini.local>
2025-07-02 15:51:26 +0530

    add swapprovider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
 import { Toaster } from "@/components/Toaster";
+import SwapProvider from "@/components/SwapProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,11 +30,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="mx-auto min-h-screen bg-[#FFC882] relative overflow-hidden">
-          {children}
-          <Footer />
-          <Toaster />
-        </div>
+        <SwapProvider>
+          <div className="mx-auto min-h-screen bg-[#FFC882] relative overflow-hidden">
+            {children}
+            <Footer />
+            <Toaster />
+          </div>
+        </SwapProvider>
       </body>
     </html>
   );
---
+++ components/SwapProvider.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { WagmiProvider } from "wagmi";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { config } from "@/lib/wagmi";
+
+const queryClient = new QueryClient();
+
+const SwapProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>{children}</RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+};
+
+export default SwapProvider;
